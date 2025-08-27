662242c64c370bb639a46fba37865b6c7cdb5266
dacer521 <140218311+dacer521@users.noreply.github.com>
2025-08-27 11:40:57 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',  # change in production
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Root route
+    @app.route('/')
+    def index():
+        return "Welcome to your Flask app template!"
+
+    # Example route
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000, debug=True)
