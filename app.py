cd011807595fa87209970b27c14eb1885ee06a7d
QuirkyGuy <129766022+QuirkyGuy@users.noreply.github.com>
2025-09-11 11:22:49 -0400

    Import sqlalchemy
    And flask_login and werkzeug.security (we might not need this)

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
 from flask import Flask, render_template
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
+#we might not need this one
+from werkzeug.security import generate_password_hash, check_password_hash
+
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',  # change in production
         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+        SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite",
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
     )
 
     if test_config is None:
