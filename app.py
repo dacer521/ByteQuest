8fcfe89bab7519477aa95bdeb669d3203f85be24
QuirkyGuy <129766022+QuirkyGuy@users.noreply.github.com>
2025-09-25 11:41:05 -0400

    Fix import error
    Database should also be initialized automatically

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import os
 import sqlite3
 from flask import Flask, render_template
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from flask_wtf import *; #this is giving errors right now. commenting it out until i figure that out,
-
-#we might not need this one
+from flask_wtf import *;
 from werkzeug.security import generate_password_hash, check_password_hash
-
 from oauthlib.oauth2 import WebApplicationClient
 import requests
 
@@ -17,44 +13,18 @@ from db import init_db_command
 from user import User
 
 # Configuration
-"""Important to note we are not storing client_secret here. ID is technically fine but, still,
-for these, you can open CMD prompt and do:
-set GOOGLE_CLIENT_(secret or id)=whatever_the_keys_are
-"""
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None) 
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
-
-# User session management setup
-# https://flask-login.readthedocs.io/en/latest
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Naive database setup
-try:
-    init_db_command()
-except sqlite3.OperationalError:
-    # Assume it's already been created
-    pass
-
-# OAuth 2 client setup (for the google logon)
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
-# Flask-Login helper to retrieve a user from the db
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY=os.environ.get("SECRET_KEY") or os.urandom(24), #makes sure it's always encrypted even if secret key isn't set as an environmental variable 
+        SECRET_KEY=os.environ.get("SECRET_KEY") or os.urandom(24),
         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-        SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite",
-        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        SQLALCHEMY_DATABASE_URI="sqlite:///db.sqlite",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
     if test_config is None:
@@ -67,21 +37,28 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-###################################################################################################################################################
-    """These should only be essential routes. We will deal with articles' routes using code to do it automatically"""
+    # User session management setup
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
-    # Root route
+    client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.get(user_id)
+
+    # Ensure the database is initialized manually via CLI
+    app.cli.add_command(init_db_command)
+
+    """These should only be essential routes"""
     @app.route('/')
     def index():
         return render_template("index.html")
 
-    # Example route
     @app.route('/home')
-    #login_required makes it so they need to be logged in, in this case have a linked email. we wil use this for pretty much everywhere except login.
     @login_required
     def hello():
         return 'Hello, World!'
-    
 
     return app
 
