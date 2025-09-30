98dfdb587aa239b160ef4d5e268c80cfe0bf38f7
QuirkyGuy <chillestnye+github-QuirkyGuy@gmail.com>
2025-09-29 21:32:37 -0400

    Finally communicate to Google Servers
    Returns an error but will fix.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_wtf import *;
@@ -50,11 +50,41 @@ def create_app(test_config=None):
     # Ensure the database is initialized manually via CLI
     app.cli.add_command(init_db_command)
 
+    #TODO: should probably add error handling to this
+    def get_google_provider_cfg():
+        return requests.get(GOOGLE_DISCOVERY_URL).json();
     """These should only be essential routes"""
     @app.route('/')
     def index():
-        return render_template("index.html")
+        if current_user.is_authenticated:
+            return (
+                "<p>Hello, {}! You're logged in! Email: {}</p>"
+                "<div><p>Google Profile Picture:</p>"
+                '<img src="{}" alt="Google profile pic"></img></div>'
+                '<a class="button" href="/logout">Logout</a>'.format(
+                    current_user.name, current_user.email, current_user.profile_pic
+                )
+            )
+        else:
+            return '<a class="button" href="/login">Google Login</a>'
 
+            # return render_template("index.html") #TODO: UNCOMMENT AND TURN ABOVE INTO A TEMPLATE
+    
+    @app.route("/login")
+    def login():
+        # Find out what URL to hit for Google login
+        google_provider_cfg = get_google_provider_cfg()
+        authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+        # Use library to construct the request for Google login and provide
+        # scopes that let you retrieve user's profile from Google
+        request_uri = client.prepare_request_uri(
+            authorization_endpoint,
+            redirect_uri=request.base_url + "/callback",
+            scope=["openid", "email", "profile"],
+        )
+        return redirect(request_uri)
+    
     @app.route('/home')
     @login_required
     def hello():
