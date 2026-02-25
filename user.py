9fa24a9a3215147678e599ebcd493a0c50ada2ff
QuirkyGuy <129766022+QuirkyGuy@users.noreply.github.com>
2026-02-25 01:40:02 -0500

    Fully implement microsoft login

--- user.py
+++ user.py
@@ -32,3 +32,22 @@ class User(UserMixin):
             (id_, name, email, profile_pic),
         )
         db.commit()
+
+    @staticmethod
+    def get_by_email(email):
+        db = get_db()
+        user = db.execute(
+            "SELECT * FROM user WHERE email = ?", (email,)
+        ).fetchone()
+        if not user:
+            return None
+        return User(id_=user[0], name=user[1], email=user[2], profile_pic=user[3])
+
+    @staticmethod
+    def update_profile(id_, name, profile_pic):
+        db = get_db()
+        db.execute(
+            "UPDATE user SET name = ?, profile_pic = ? WHERE id = ?",
+            (name, profile_pic, id_),
+        )
+        db.commit()
