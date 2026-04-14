dd1c2f3d4a5555b68d188f92aff77ab353161952
QuirkyGuy <129766022+QuirkyGuy@users.noreply.github.com>
2026-04-14 11:03:26 -0400

    Add localhost comment in db.py

--- db.py
+++ db.py
@@ -8,6 +8,8 @@ from flask.cli import with_appcontext
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
+            # use this if local instead of /var/www/byte....
+            # "sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
             "/var/www/bytequest/instance/sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
